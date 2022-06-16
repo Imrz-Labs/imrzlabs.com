452d9cd3ee91144b26f46496b9274c562fe891f8
Ben Emma <emmaben676@gmail.com>
2022-06-16 16:40:39 +0100

    update __011

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -5,163 +5,179 @@ import { Footer, TopMenu } from '../components'
 const privacy = () => {
   return (
     <>
-    <Head>
+      <Head>
         <title>Imrz Labs | Privacy Policy</title>
-        <meta name="description" content="Imrz Labs is a web 3.0 company created by industry 4 veterans who have started and sold highly scaleable tech companies with the goal to make emerging technologies accessible for everyone" />
-        <meta name="keywords" content="Immersive company, immersive agency, imrz, imrz labs, imrz agency, imrz ventures, 3D, metaverse, oasis, ar, vr, xr, mr, tokenisation, mining, NFTs, DAOs, Defi, AI, Airdrops, Decentralization, Smart Contracts, Blockchain, Crypto, Web 3, Computer Vision, DApps, Phygital, Digital Twin, coins, Staking" />
+        <meta
+          name="description"
+          content="Imrz Labs is a web 3.0 company created by industry 4 veterans who have started and sold highly scaleable tech companies with the goal to make emerging technologies accessible for everyone"
+        />
+        <meta
+          name="keywords"
+          content="Immersive company, immersive agency, imrz, imrz labs, imrz agency, imrz ventures, 3D, metaverse, oasis, ar, vr, xr, mr, tokenisation, mining, NFTs, DAOs, Defi, AI, Airdrops, Decentralization, Smart Contracts, Blockchain, Crypto, Web 3, Computer Vision, DApps, Phygital, Digital Twin, coins, Staking"
+        />
         <link rel="icon" href="/imrzicon.gif" />
       </Head>
-      
-        <TopMenu />
-        <br/>
 
-    <div className='h-auto m-auto my-28 px-10 py-10 bg-white items-center justify-center rounded-3xl shadow-3xl 3xl:w-9/12 2xl:w-9/12 xl:w-11/12 lg:my-[-20px] md:my-20'>
+      <TopMenu />
+      <br />
 
-      <p className=''>
-        IMRZ Labs operates the www.IMRZlabs.com website, which provides the
-        SERVICE.
-      </p>
+      <div className="shadow-3xl m-auto my-28 h-auto items-center justify-center rounded-3xl bg-white px-10 py-10 3xl:w-9/12 2xl:w-9/12 xl:w-11/12 lg:my-[-20px] md:my-20">
+        <p className="">
+          IMRZ LABS LTD - Company Number:11332225 Registered Office Address - 10
+          Grange Road, West Kirby, Wirral, Merseyside, CH48 4HA UK Contact
+          Email: Info@IMRZLABS.com
+        </p>
 
-      <p className="py-5 font-semibold ">
-        This page is used to inform website visitors regarding our policies with
-        the collection, use, and disclosure of Personal Information if anyone
-        decided to use our Service, the Www.IMRZlabs.com website.
-      </p>
+        <p className="">
+          IMRZ Labs operates the www.IMRZlabs.com website, which provides the
+          SERVICE.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        If you choose to use our Service, then you agree to the collection and
-        use of information in relation with this policy. The Personal
-        Information that we collect are used for providing and improving the
-        Service. We will not use or share your information with anyone except as
-        described in this Privacy Policy. Our Privacy Policy was created with
-        the help of the{' '}
-        <a href="https://www.termsfeed.com/privacy-policy-generator/">
-          Privacy Policy Generator
-        </a>
-        .
-      </p>
+        <p className="py-5 font-semibold ">
+          This page is used to inform website visitors regarding our policies
+          with the collection, use, and disclosure of Personal Information if
+          anyone decided to use our Service, the Www.IMRZlabs.com website.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        The terms used in this Privacy Policy have the same meanings as in our
-        Terms and Conditions, which is accessible at www.IMRZlabs.com, unless
-        otherwise defined in this Privacy Policy.
-      </p>
+        <p className="py-5 font-semibold ">
+          If you choose to use our Service, then you agree to the collection and
+          use of information in relation with this policy. The Personal
+          Information that we collect are used for providing and improving the
+          Service. We will not use or share your information with anyone except
+          as described in this Privacy Policy. Our Privacy Policy was created
+          with the help of the{' '}
+          <a href="https://www.termsfeed.com/privacy-policy-generator/">
+            Privacy Policy Generator
+          </a>
+          .
+        </p>
 
-      <h2 className='text-base font-bold md:text-sm'>Information Collection and Use</h2>
+        <p className="py-5 font-semibold ">
+          The terms used in this Privacy Policy have the same meanings as in our
+          Terms and Conditions, which is accessible at www.IMRZlabs.com, unless
+          otherwise defined in this Privacy Policy.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        For a better experience while using our Service, we may require you to
-        provide us with certain personally identifiable information, including
-        but not limited to your name, phone number, and postal address. The
-        information that we collect will be used to contact or identify you.
-      </p>
+        <h2 className="text-base font-bold md:text-sm">
+          Information Collection and Use
+        </h2>
 
-      <h2 className='text-base font-bold md:text-sm'>Log Data</h2>
+        <p className="py-5 font-semibold ">
+          For a better experience while using our Service, we may require you to
+          provide us with certain personally identifiable information, including
+          but not limited to your name, phone number, and postal address. The
+          information that we collect will be used to contact or identify you.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        We want to inform you that whenever you visit our Service, we collect
-        information that your browser sends to us that is called Log Data. This
-        Log Data may include information such as your computer’s Internet
-        Protocol ("IP") address, browser version, pages of our Service that you
-        visit, the time and date of your visit, the time spent on those pages,
-        and other statistics.
-      </p>
+        <h2 className="text-base font-bold md:text-sm">Log Data</h2>
 
-      <h2 className='text-base font-bold md:text-sm'>Cookies</h2>
+        <p className="py-5 font-semibold ">
+          We want to inform you that whenever you visit our Service, we collect
+          information that your browser sends to us that is called Log Data.
+          This Log Data may include information such as your computer’s Internet
+          Protocol ("IP") address, browser version, pages of our Service that
+          you visit, the time and date of your visit, the time spent on those
+          pages, and other statistics.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        Cookies are files with small amount of data that is commonly used an
-        anonymous unique identifier. These are sent to your browser from the
-        website that you visit and are stored on your computer’s hard drive.
-      </p>
+        <h2 className="text-base font-bold md:text-sm">Cookies</h2>
 
-      <p className="py-5 font-semibold ">
-        Our website uses these "cookies" to collection information and to
-        improve our Service. You have the option to either accept or refuse
-        these cookies, and know when a cookie is being sent to your computer. If
-        you choose to refuse our cookies, you may not be able to use some
-        portions of our Service.
-      </p>
+        <p className="py-5 font-semibold ">
+          Cookies are files with small amount of data that is commonly used an
+          anonymous unique identifier. These are sent to your browser from the
+          website that you visit and are stored on your computer’s hard drive.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        For more general information on cookies, please read{' '}
-        <a href="https://www.termsfeed.com/blog/sample-cookies-policy-template/#What_Are_Cookies">
-          "Cookies" article from the TermsFeed website
-        </a>
-        .
-      </p>
+        <p className="py-5 font-semibold ">
+          Our website uses these "cookies" to collection information and to
+          improve our Service. You have the option to either accept or refuse
+          these cookies, and know when a cookie is being sent to your computer.
+          If you choose to refuse our cookies, you may not be able to use some
+          portions of our Service.
+        </p>
 
-      <h2 className='text-base font-bold md:text-sm'>Service Providers</h2>
+        <p className="py-5 font-semibold ">
+          For more general information on cookies, please read{' '}
+          <a href="https://www.termsfeed.com/blog/sample-cookies-policy-template/#What_Are_Cookies">
+            "Cookies" article from the TermsFeed website
+          </a>
+          .
+        </p>
 
-      <p className="py-5 font-semibold ">
-        We may employ third-party companies and individuals due to the following
-        reasons:
-      </p>
+        <h2 className="text-base font-bold md:text-sm">Service Providers</h2>
 
-      <ul>
-        <li>To facilitate our Service;</li>
-        <li>To provide the Service on our behalf;</li>
-        <li>To perform Service-related services; or</li>
-        <li>To assist us in analyzing how our Service is used.</li>
-      </ul>
+        <p className="py-5 font-semibold ">
+          We may employ third-party companies and individuals due to the
+          following reasons:
+        </p>
 
-      <p className="py-5 font-semibold ">
-        We want to inform our Service users that these third parties have access
-        to your Personal Information. The reason is to perform the tasks
-        assigned to them on our behalf. However, they are obligated not to
-        disclose or use the information for any other purpose.
-      </p>
+        <ul>
+          <li>To facilitate our Service;</li>
+          <li>To provide the Service on our behalf;</li>
+          <li>To perform Service-related services; or</li>
+          <li>To assist us in analyzing how our Service is used.</li>
+        </ul>
 
-      <h2 className='text-base font-bold md:text-sm'>Security</h2>
+        <p className="py-5 font-semibold ">
+          We want to inform our Service users that these third parties have
+          access to your Personal Information. The reason is to perform the
+          tasks assigned to them on our behalf. However, they are obligated not
+          to disclose or use the information for any other purpose.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        We value your trust in providing us your Personal Information, thus we
-        are striving to use commercially acceptable means of protecting it. But
-        remember that no method of transmission over the internet, or method of
-        electronic storage is 100% secure and reliable, and we cannot guarantee
-        its absolute security.
-      </p>
+        <h2 className="text-base font-bold md:text-sm">Security</h2>
 
-      <h2 className='text-base font-bold md:text-sm'>Links to Other Sites</h2>
+        <p className="py-5 font-semibold ">
+          We value your trust in providing us your Personal Information, thus we
+          are striving to use commercially acceptable means of protecting it.
+          But remember that no method of transmission over the internet, or
+          method of electronic storage is 100% secure and reliable, and we
+          cannot guarantee its absolute security.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        Our Service may contain links to other sites. If you click on a
-        third-party link, you will be directed to that site. Note that these
-        external sites are not operated by us. Therefore, we strongly advise you
-        to review the Privacy Policy of these websites. We have no control over,
-        and assume no responsibility for the content, privacy policies, or
-        practices of any third-party sites or services.
-      </p>
+        <h2 className="text-base font-bold md:text-sm">Links to Other Sites</h2>
 
-      <p className="py-5 font-semibold ">Children's Privacy</p>
+        <p className="py-5 font-semibold ">
+          Our Service may contain links to other sites. If you click on a
+          third-party link, you will be directed to that site. Note that these
+          external sites are not operated by us. Therefore, we strongly advise
+          you to review the Privacy Policy of these websites. We have no control
+          over, and assume no responsibility for the content, privacy policies,
+          or practices of any third-party sites or services.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        Our Services do not address anyone under the age of 13. We do not
-        knowingly collect personal identifiable information from children under
-        13. In the case we discover that a child under 13 has provided us with
-        personal information, we immediately delete this from our servers. If
-        you are a parent or guardian and you are aware that your child has
-        provided us with personal information, please contact us so that we will
-        be able to do necessary actions.
-      </p>
+        <p className="py-5 font-semibold ">Children's Privacy</p>
 
-      <h2 className='text-base font-bold md:text-sm'>Changes to This Privacy Policy</h2>
+        <p className="py-5 font-semibold ">
+          Our Services do not address anyone under the age of 13. We do not
+          knowingly collect personal identifiable information from children
+          under 13. In the case we discover that a child under 13 has provided
+          us with personal information, we immediately delete this from our
+          servers. If you are a parent or guardian and you are aware that your
+          child has provided us with personal information, please contact us so
+          that we will be able to do necessary actions.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        We may update our Privacy Policy from time to time. Thus, we advise you
-        to review this page periodically for any changes. We will notify you of
-        any changes by posting the new Privacy Policy on this page. These
-        changes are effective immediately, after they are posted on this page.
-      </p>
+        <h2 className="text-base font-bold md:text-sm">
+          Changes to This Privacy Policy
+        </h2>
 
-      <h2 className='text-base font-bold md:text-sm'>Contact Us</h2>
+        <p className="py-5 font-semibold ">
+          We may update our Privacy Policy from time to time. Thus, we advise
+          you to review this page periodically for any changes. We will notify
+          you of any changes by posting the new Privacy Policy on this page.
+          These changes are effective immediately, after they are posted on this
+          page.
+        </p>
 
-      <p className="py-5 font-semibold ">
-        If you have any questions or suggestions about our Privacy Policy, do
-        not hesitate to contact us.
-      </p>
-     </div>
-     <Footer />
+        <h2 className="text-base font-bold md:text-sm">Contact Us</h2>
+
+        <p className="py-5 font-semibold ">
+          If you have any questions or suggestions about our Privacy Policy, do
+          not hesitate to contact us.
+        </p>
+      </div>
+      <Footer />
     </>
   )
 }
